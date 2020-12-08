e65318dac875da71388840b878ac59800d3b3e5c
CarlosQool <65362893+CarlosQool@users.noreply.github.com>
2020-12-07 20:01:50 -0400

    Add files via upload

---
+++ qrCodeScanner.js
@@ -0,0 +1,65 @@
+var qrcode = window.qrcode;
+
+var video = document.createElement("video");
+var canvasElement = document.getElementById("qr-canvas");
+var canvas = canvasElement.getContext("2d");
+
+var qrResult = document.getElementById("qr-result");
+var outputData;
+var btnScanQR = document.getElementById("ShareSetting1");
+
+var scanning = false;
+
+qrcode.callback = (res) => {
+  if (res) {
+    outputData = res;
+    scanning = false;
+
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
+
+    qrResult.hidden = false;
+    btnScanQR.hidden = false;
+    canvasElement.hidden = true;
+  }
+};
+
+btnScanQR.onclick = () =>
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
+
+
+
+
+
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
+}
+
+
+
+
+
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
+}
